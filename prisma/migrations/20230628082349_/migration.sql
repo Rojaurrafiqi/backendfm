6cd7e9da8ca204a896c6938318973a1ac569591a
natieq <natieq@briit.co.id>
2023-06-30 15:23:34 +0700

    fixing pasien tabel

---
+++ prisma/migrations/20230628082349_/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `gender` (
+    `id_gender` INTEGER NOT NULL AUTO_INCREMENT,
+    `jenis_kelamin` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id_gender`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
