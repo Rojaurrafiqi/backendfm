f511cff116576f9b39290bb52f6ddb9326114788
natieq <natieq@briit.co.id>
2023-07-07 17:44:23 +0700

    fixing pemeriksaan penunjang

--- SQL All/barang_grup_202307021120.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO filemedis_new.barang_grup (grup) VALUES
-	 ('gudang farmasi'),
-	 ('gudang umum');
--- SQL All/gender_202307021120.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO filemedis_new.gender (id_gender,jenis_kelamin) VALUES
-	 (0,'tidak didata'),
-	 (1,'laki-laki'),
-	 (2,'perempuan');
